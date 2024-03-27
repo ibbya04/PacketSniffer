75a66d74c29000634b8ff4a69d990e78daba3f80
ibbya04 <ibraheem.m.a04@gmail.com>
2024-03-27 16:29:20 +0000

    Added functions
    Added get_mac_address function to format MAC addresses. Added a main function.

--- PacketSniffer.py
+++ PacketSniffer.py
@@ -1,6 +1,18 @@
 import socket
-from struct import *
+import struct
 import sys
+
+
+def main():
+    # # creates a raw socket capable of capturing TCP packets at IP level.
+    s = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_TCP)
+
+    # infinite loop to recieve data from socket, then formats and prints data
+    while True:
+        raw_data, addr = s.recvfrom(65535)
+        dest_mac, src_mac, proto, data = ethernet_head(raw_data)
+        print('Ethernet Frame:')
+        print('Destination: {}, Source: {}, Protocol: {}'.format(dest_mac, src_mac, proto))
 
 def ethernet_head(raw_data):
     # unpacks binary data. ! specifies big endian as network data is big endian.
@@ -17,10 +29,14 @@ def ethernet_head(raw_data):
     data = raw_data[14:]
     return dest_mac, src_mac, proto, data
 
-# creates a raw socket capable of capturing TCP packets at IP level.
-s = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_TCP)
+# converts bytes into formatted MAC address
+def get_mac_addr(bytes):
+    # formats each byte without need for a loop
+    bytes_str = map('{:02x}'.format, bytes)
+    # joins bytes together wiht ':' inbetween and converts all to uppercase
+    mac_address = ':'.join(bytes_str).upper()
+    return mac_address
 
-# infinite loop to recieve data from socket
-while True:
-    print(s.recvfrom(65565))
+
+main()
     
